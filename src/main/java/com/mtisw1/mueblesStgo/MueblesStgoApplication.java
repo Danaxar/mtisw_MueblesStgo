7c7e474f3ced43845de17c427fd6054c38cc0621
Daniel Catalán <daniel.catalan@usach.cl>
2022-09-02 14:41:32 -0400

    First commit

---
+++ src/main/java/com/mtisw1/mueblesStgo/MueblesStgoApplication.java
@@ -0,0 +1,13 @@
+package com.mtisw1.mueblesStgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MueblesStgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MueblesStgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtisw1/mueblesStgo/MueblesStgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtisw1.mueblesStgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MueblesStgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
